44dca51fa326b002bba1fae5633fe58296788678
ShravaniMishra <shravniguddi@gmail.com>
2023-01-20 12:05:18 +0530

    bubblesort

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,18 +1,17 @@
-function solve(N,arr){
-    //write code here
-    for(let i=0;i<N;i++){
-        for(let j=0;j<N;j++){
-            if(arr[j]>arr[j+1]) {
-                swap(arr,j,j+1)
-            }
-           
+let arr = [8,2,5,1,7,6]
+for(let i=0;i<arr.length;i++){
+    for(let j=0;j<arr.length;j++){
+        if(arr[j]>arr[j+1]){
+            swap(arr,j,j+1)
         }
-       
     }
-     console.log(arr.join(" "))
-}
+} 
+console.log(arr.join(" "))
+
+
+
 function swap(arr,i,j){
-    let temp=arr[i]
+    let temp = arr[i]
     arr[i]=arr[j]
     arr[j]=temp
 }
