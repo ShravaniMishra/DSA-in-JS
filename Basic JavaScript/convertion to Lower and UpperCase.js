14c20ade9ffde19cf22e643694a025f312201579
ShravaniMishra <shravniguddi@gmail.com>
2023-01-24 15:48:50 +0530

    Basic concept is added

---
+++ Basic JavaScript/convertion to Lower and UpperCase.js
@@ -0,0 +1,12 @@
+let str =  "shravani"
+console.log(str.toUpperCase())
+
+
+// let str = "ShravaNi"
+// console.log(str.toLowerCase())
+
+// let str = "shravaNi"
+// console.log(str.toLocaleUpperCase())
+
+// let str = "ShrAvaNi"
+// console.log(str.toLocaleLowerCase())
